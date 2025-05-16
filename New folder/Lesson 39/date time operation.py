e9ccacf767d70d5411ce09f2f7d708ec675c684f
MahirRAJ577 <mahirrajkumar12@gmail.com>
2025-05-15 20:38:25 -0400

    Lesson 39

---
+++ New folder/Lesson 39/date time operation.py
@@ -0,0 +1,13 @@
+import datetime
+
+# using now() to get the current time
+current_time = datetime.datetime.now()
+
+# printing value of now.
+print("Time now at greenwich meridian is : ", end = "")
+print(current_time)
+
+# print calender of year 2025
+import calendar
+
+print("\n", calendar.calendar(2025))
