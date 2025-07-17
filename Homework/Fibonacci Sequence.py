95ddc45742d7d0c866af8b9a924c2ee7756f116a
MahirRAJ577 <mahirrajkumar12@gmail.com>
2025-07-17 14:16:15 -0400

    Fibonacci sequence

--- Homework/Fibonacci Sequence.py
+++ Homework/Fibonacci Sequence.py
@@ -1,15 +1,22 @@
 def fibsequence(n):
-
+    
     a=0
     b=1
-    print(a)
-    print(b)
+   
+    if n <= 1:
+        print (a)
     
-    for i in range(2,n):
-        
-        c = a+b
-        a=b
-        b=c
-        print(a+b)
+    else:
+        print(a)
+        print(b)
+    
+        for i in range(2,n):
 
-fibsequence(15)
+            c = a + b
+            a=b
+            b=c
+            print(a+b)
+
+num = int(input("How many numbers would you like in the Fibonacci sequence : "))
+
+fibsequence(num)
