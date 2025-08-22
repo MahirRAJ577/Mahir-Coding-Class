eb7450f4b39f659702850b5f83d61bb525ec26d6
MahirRAJ577 <mahirrajkumar12@gmail.com>
2025-08-21 20:42:01 -0400

    Lesson 44

---
+++ Homework/Random number print.js
@@ -0,0 +1,8 @@
+
+let x = Math.floor(Math.random() * 6) + 1;
+let y = Math.floor(Math.random() * 6) + 1;
+let z = Math.floor(Math.random() * 6) + 1;
+
+console.log(x);
+console.log(y);
+console.log(z);
