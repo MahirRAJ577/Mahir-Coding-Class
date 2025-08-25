0643949a0b6ada4be0113992cf7f4274b91f7288
MahirRAJ577 <mahirrajkumar12@gmail.com>
2025-08-25 16:25:00 -0400

    1

--- Homework/age validation.js
+++ Homework/age validation.js
@@ -1,9 +1,5 @@
-let num1 = prompt("Please enter your age")
-let name = prompt("Please enter your name")
-
-if (num1 >= 18) {
-    console.log("You are eligible to enter this form")
-}
-else {
-    console.log("You are not eligible to enter this form")
+if (confirm("Are you at least 18 years old?")) {
+    alert("Access granted!");
+} else {
+    alert("You are not allowed.");
 }
