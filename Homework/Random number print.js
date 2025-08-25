9b152ffc1764506475774bbd8a77dc955264938c
MahirRAJ577 <mahirrajkumar12@gmail.com>
2025-08-24 22:31:58 -0400

    random printing

--- Homework/Random number print.js
+++ Homework/Random number print.js
@@ -1,8 +1,9 @@
+let x = Math.floor(Math.random() * 100) + 1;
+let y = Math.floor(Math.random() * 100) + 1;
+let z = Math.floor(Math.random() * 100) + 1;
 
-let x = Math.floor(Math.random() * 6) + 1;
-let y = Math.floor(Math.random() * 6) + 1;
-let z = Math.floor(Math.random() * 6) + 1;
-
-console.log(x);
-console.log(y);
-console.log(z);
+while(x==0){
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
---
+++ Homework/random numberptinting.js
@@ -0,0 +1,8 @@
+let count = 10;  
+let i = 0;
+
+while (i < count) {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber);
+    i++;
+}
