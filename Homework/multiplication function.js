2d20ea0eece3d8eb76a648354e9b6bc297466020
MahirRAJ577 <mahirrajkumar12@gmail.com>
2025-08-25 16:14:06 -0400

    1

--- Homework/multiplication function.js
+++ Homework/multiplication function.js
@@ -3,9 +3,9 @@ function multiplynum(num1, num2) {
 }
 
 function multiplication(a, b){
+    let num1 = parseFloat("Enter the first number you would like to multiply")
+    let num2 = parseFloat("Enter the second number you would like to multiply")
     let result = multiplynum(a, b);
     console.log(`The result of multiplying ${a} and ${b} is: ${result}`);
 }
 
-let num1 = prompt("Enter the first number you would like to multiply")
-let num2 = prompt("Enter the second number you would like to multiply")
